f0bbea6437a8c8504c518721d17b14296302c8aa
akshay <akshaychakale@gmail.com>
2024-10-06 23:22:08 +0530

    Update RetryAnalyzer.java

--- src/main/java/utils/RetryAnalyzer.java
+++ src/main/java/utils/RetryAnalyzer.java
@@ -6,6 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
  //This utility class is created to retry the failed tests
     int count=0;
+    //retryCount variable is to declare the number of times the test will re-run if failed
     int retryCount=1;
 
     @Override
